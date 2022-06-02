fad1ae55038c16fbe216bd1745e5442a4ee6c497
haechan <kiki9510@gmail.com>
2022-06-02 16:14:56 +0900

    chore: cors설정

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { errorMiddleware } from "./middlewares/errorHandler";
 import "dotenv/config";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorMiddleware);
