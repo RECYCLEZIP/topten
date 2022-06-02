508bc7f0c2588acca266aad73223e8bcdfd7168b
haechan <kiki9510@gmail.com>
2022-06-02 15:46:00 +0900

    style: eslint, prettier 관련 설정파일 추가

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { errorMiddleware } from "./middlewares/errorHandler";
 import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send("AI Project 10 TEAM - TOPTEN");
