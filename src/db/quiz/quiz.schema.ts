34989783639080afc62ad5045e02a75b7513eb10
haechan <kiki9510@gmail.com>
2022-06-03 12:37:29 +0900

    [#5] feat: QuizSchema 작성

---
+++ src/db/quiz/quiz.schema.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const QuizSchema = new Schema({
+    QuizId: {
+        type: Schema.Types.ObjectId,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    options: {
+        type: Array,
+        required: true,
+        defualt: [],
+    },
+    answer: {
+        type: String,
+        required: true,
+    },
+    result: {
+        type: Object,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+});
+
+export const QuizModel = model("Quiz", QuizSchema);
