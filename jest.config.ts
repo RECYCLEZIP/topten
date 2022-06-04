b037471facf62f412eff1ac2ce28b906bfa21803
haechan <kiki9510@gmail.com>
2022-06-04 11:23:47 +0900

    test: test 코드 및 관련 설정 수정

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,13 @@
+import type { Config } from "@jest/types";
+import { defaults } from "jest-config";
+
+// Or async function
+export default async (): Promise<Config.InitialOptions> => {
+    return {
+        verbose: true,
+    };
+};
+
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
