39d78cc9289ad743e7aa87565c8cf23175042991
handwoong <handwoong@gmail.com>
2022-06-05 15:00:25 +0900

    style: eslint-prettier 포맷 적용

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,11 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-const errorMiddleware = (
-    error: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
+const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
     console.log("\x1b[33m%s\x1b[0m", error);
     res.status(400).send(error.message);
 };
