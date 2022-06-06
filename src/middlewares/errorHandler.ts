d1fe1914c3a1a26428c69973d6b5e216ed404e13
handwoong <handwoong@gmail.com>
2022-06-07 01:41:57 +0900

    [#4] chore: eslint-no-unused-vars 예외 설정

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
 
-const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
+const errorMiddleware: ErrorRequestHandler = (error, req, res, _next) => {
     console.log("\x1b[33m%s\x1b[0m", error);
     res.status(400).send(error.message);
 };
