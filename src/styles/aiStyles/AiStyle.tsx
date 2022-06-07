90bc897c0d14ac2240b7ca8533bcb2709a315850
aeebbr <aeebbr@gmail.com>
2022-06-07 20:38:59 +0900

    [#9] feat: Styled ai

---
+++ src/styles/aiStyles/AiStyle.tsx
@@ -0,0 +1,127 @@
+import styled from "styled-components";
+
+import { Button } from "../ButtonStyles";
+
+export const AiTopTitle = styled.span`
+  font-size: 1rem;
+`;
+
+export const AiImageUploadSection = styled.div`
+  margin: 0 3rem;
+`;
+
+export const AiImageContainer = styled.div`
+  background: #9eacba;
+
+  width: 100%;
+  height: 10rem;
+
+  display: flex;
+  justify-content: center;
+
+  border-radius: 0.5rem;
+`;
+export const AiImageWrapper = styled.div`
+  /* background: red;
+
+  width: 80%;
+  height: auto; */
+`;
+
+export const AiImage = styled.img`
+  background: blue;
+
+  width: 100%;
+`;
+
+export const AiIconsContainer = styled.div`
+  background: goldenrod;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const AiIcon = styled.img`
+  width: 1rem;
+  height: auto;
+
+  margin-right: 0.2rem;
+`;
+
+export const AiTopContainer = styled.div`
+  background: green;
+
+  margin: 0 1rem;
+  margin-bottom: 2rem;
+
+  display: flex;
+`;
+
+export const AiTitleWrapper = styled.div`
+  background: gray;
+`;
+
+export const AiButtonWrapper = styled.div`
+  background: brown;
+
+  flex-grow: 1;
+`;
+
+export const AiButton = styled(Button)`
+  float: right;
+
+  border-radius: 0.5rem;
+`;
+
+export const AiGuideLikeSection = styled.div`
+  background:lightyellow;
+
+  width: 100%;
+
+  margin-bottom: 1rem;
+`;
+
+export const AiGuidesContainer = styled.div`
+  width: 100%;
+
+  /* padding: 1rem; */
+  display: grid;
+  justify-items: center;
+
+  background: #ffffff;
+  border: 1px solid #9eacba;
+  border-radius: 1rem;
+`;
+
+export const AiGuidesTitle = styled.span`
+  font-size: 0.8rem;
+
+  color: #9eacba;
+`;
+
+export const AiGuideContainer = styled.div`
+  background: green;
+
+  width: fit-content;
+
+  margin: 1rem;
+`;
+
+export const AiGuideImgWrapper = styled.div`
+  background: lightblue;
+
+  width: 30%;
+
+  margin: auto;
+
+  text-align: -webkit-center;
+`;
+
+export const AiGuideTitle = styled.span`
+  font-size: 0.6rem;
+`;
+export const AiGuideDescription = styled.span`
+  //
+
+  font-weight: 400;
+`;
