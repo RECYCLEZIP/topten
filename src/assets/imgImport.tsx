ad28a875388e9cf6e30c7d659c07ae9d3cefc408
ms <altjsvnf@naver.com>
2022-06-08 01:46:13 +0900

    chore: img folder structure

--- src/assets/imgImport.tsx
+++ src/assets/imgImport.tsx
@@ -1,14 +1,14 @@
+import seoul from "./img/seoul.jpg";
+import mainAi from "./img/ai.png";
+import PET from "./img/PET.jpg";
+import GLASS from "./img/GLASS.png";
+import PETs from "./img/PETs.jpg";
+import QuizLogo from "./img/QuizLogo.png";
+
 export const img = {
-  seoul: "/seoul.jpg",
-  mainAI: "/ai.PNG",
-  category: [
-    "./PET.jpg",
-    "./GLASS.png",
-    "./PET.jpg",
-    "./GLASS.png",
-    "./PET.jpg",
-    "./GLASS.png",
-    "./PET.jpg",
-    "./GLASS.png",
-  ],
+  seoul: seoul,
+  mainAI: mainAi,
+  category: [PET, GLASS, PET, GLASS, PET, GLASS, PET, GLASS],
+  sample: PETs,
+  quizLogo: QuizLogo,
 };
