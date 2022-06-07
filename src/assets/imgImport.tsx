cf4ab4f9ec2fcdecfd7c69013de8e50358aa49cf
aeebbr <aeebbr@gmail.com>
2022-06-07 20:38:00 +0900

    [#9] Added icons of ai page

--- src/assets/imgImport.tsx
+++ src/assets/imgImport.tsx
@@ -1,4 +1,6 @@
 export const img = {
-    bin: `/img/bins.png`,
-    map: `/img/map.png`,
+  bin: `/img/bins.png`,
+  map: `/img/map.png`,
+  camera: `/img/Camera.png`,
+  image: `/img/Image.png`,
 };
