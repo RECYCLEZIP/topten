7b2564176438f53b67ea3981a979b9025cc47f74
aeebbr <aeebbr@gmail.com>
2022-06-07 18:47:43 +0900

    [#4] feat: Creating Ai.tsx, Setting constructure

---
+++ src/pages/ai/Ai.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Ai() {
+  return (
+    <>
+      <div>Ai 분석하기</div>
+      {/* 사진, 카메라 */}
+      <div>
+        {/* 사진 */}
+        <div></div>
+        {/* 아이콘 */}
+        <div></div>
+      </div>
+      {/* 사진 업로드 문구, 분석하기 버튼 */}
+      <div>
+        {/* 사진 업로드 문구 */}
+        <div>사진을 업로드하세요</div>
+        <button>분석하기</button>
+      </div>
+      {/* 이런 사진이 좋아요! */}
+      <div>
+        {/* 타이틀 */}
+        <div>이런 사진이 좋아요!</div>
+        {/* 사진 예시 섹션 */}
+
+        <div>
+          {/* 사진 1 섹션*/}
+          <div>
+              {/* 사진 1 래퍼 */}
+              <div>
+
+              </div>
+              {/* 사진 1 설명 */}
+              <div>
+                  화질이 선명한 사진
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Ai;
