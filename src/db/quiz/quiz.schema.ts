67a2800984917bf88419b9ed97d46c06d655a6b0
handwoong <handwoong@gmail.com>
2022-06-08 01:07:46 +0900

    fix: 퀴즈 정답 enum타입 추가

--- src/db/quiz/quiz.schema.ts
+++ src/db/quiz/quiz.schema.ts
@@ -34,7 +34,7 @@ const QuizSchema = new Schema<Quiz>({
     answer: {
         type: String,
         required: true,
-        enum: ["1", "2", "3", "o", "x", "음식물", "일반"],
+        enum: ["1", "2", "3", "4", "o", "x", "음식물", "일반"],
     },
     result: {
         type: [{ date: Date, totalUser: Number, wrong: Number, yesterday: Number }],
