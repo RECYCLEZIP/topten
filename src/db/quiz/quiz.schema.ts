392c474d416c0a72c22949fe3f5ae38db3e80dae
handwoong <handwoong@gmail.com>
2022-06-07 13:11:43 +0900

    [#5] feat: 스키마에 타입 추가

--- src/db/quiz/quiz.schema.ts
+++ src/db/quiz/quiz.schema.ts
@@ -1,7 +1,24 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 
-const QuizSchema = new Schema({
-    QuizId: {
+interface Result {
+    date: Date;
+    totalUser: number;
+    wrong: number;
+    yesterday: number;
+}
+
+interface Quiz {
+    quizId: Types.ObjectId;
+    title: string;
+    options: string[];
+    answer: string;
+    result: Result[];
+    type: string;
+    image: string;
+}
+
+const QuizSchema = new Schema<Quiz>({
+    quizId: {
         type: Schema.Types.ObjectId,
     },
     title: {
@@ -9,26 +26,31 @@ const QuizSchema = new Schema({
         required: true,
     },
     options: {
-        type: Array,
+        type: [String],
         required: true,
         defualt: [],
     },
     answer: {
         type: String,
         required: true,
+        enum: ["1", "2", "3", "o", "x"],
     },
     result: {
-        type: Object,
+        type: [{ date: Date, totalUser: Number, wrong: Number, yesterday: Number }],
         required: true,
+        default: [{ date: new Date(), totalUser: 0, wrong: 0, yesterday: 0 }],
     },
     type: {
         type: String,
         required: true,
+        enum: ["multipleChoice", "mixUp", "ox"],
     },
     image: {
         type: String,
         required: true,
+        default:
+            "https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
     },
 });
 
-export const QuizModel = model("Quiz", QuizSchema);
+export const QuizModel = model<Quiz>("Quiz", QuizSchema);
