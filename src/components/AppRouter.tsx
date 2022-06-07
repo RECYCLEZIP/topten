3053421b5bd34e83e122adf5630bdc512c79b903
aeebbr <aeebbr@gmail.com>
2022-06-07 20:36:55 +0900

    [#9] feat: Routed ai page

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
 import Landing from "../pages/prologue/Prologue";
+import Ai from "../pages/ai/Ai";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/prologue" element={<Landing />} />
+      <Route path="/ai" element={<Ai />} />
     </Routes>
   );
 }
