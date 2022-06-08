3e61e8d3c749cd366f01b6319439892f96ec2bd0
aeebbr <aeebbr@gmail.com>
2022-06-08 17:39:23 +0900

    [#9] style: Renamed titleText -> notice of AiImageUpload

--- src/pages/ai/AiImageUpload.tsx
+++ src/pages/ai/AiImageUpload.tsx
@@ -14,7 +14,7 @@ import {
   AiIconsContainer,
   AiIcon,
   AiTopContainer,
-  AiTitleWrapper,
+  AiNoticeWrapper,
   AiButtonWrapper,
   AiButton,
 } from "../../styles/aiStyles/AiStyle";
@@ -25,7 +25,7 @@ function AiImageUpload() {
   // 이미지 업로드 전: beforeImgUpload, 이미지 업로드: imgUploaded, 분석 중: analyzing, 분석 완료: done
   const [situation, setSituation] = useRecoilState(AiSituationState);
 
-  const [titleText, setTitleText] = useState("");
+  const [notice, setNotice] = useState("");
   const [buttonText, setButtonText] = useState("");
 
   const imgInput = useRef<any>();
@@ -62,16 +62,16 @@ function AiImageUpload() {
   };
 
   useEffect(() => {
-    const titleText = () => {
+    const notice = () => {
       switch (situation) {
         case "imgUploaded":
-          return setTitleText("분석하기 버튼을 클릭해주세요");
+          return setNotice("분석하기 버튼을 클릭해주세요");
         case "analyzing":
-          return setTitleText("AI가 사진을 분석하고 있습니다");
+          return setNotice("AI가 사진을 분석하고 있습니다");
         case "done":
-          return setTitleText("분석이 완료되었습니다");
+          return setNotice("분석이 완료되었습니다");
         default:
-          return setTitleText("쓰레기 사진을 업로드해주세요");
+          return setNotice("쓰레기 사진을 업로드해주세요");
       }
 
       // //   switch (situation) {
@@ -96,7 +96,7 @@ function AiImageUpload() {
       //   }
     };
 
-    titleText();
+    notice();
 
     const buttonText = () => {
       if (situation === "done") {
@@ -109,9 +109,7 @@ function AiImageUpload() {
     buttonText();
   }, [situation]);
 
-  console.log(
-    `${situation}`,
-  );
+  console.log(`${situation}`);
 
   return (
     <>
@@ -145,7 +143,7 @@ function AiImageUpload() {
       {/* 사진 업로드 문구, 분석하기 버튼 */}
       <AiTopContainer>
         {/* 사진 업로드 문구 */}
-        <AiTitleWrapper>{titleText}</AiTitleWrapper>
+        <AiNoticeWrapper>{notice}</AiNoticeWrapper>
         <AiButtonWrapper>
           <AiButton
             // 버튼 비활성화: 업로드 전, 분석 중
