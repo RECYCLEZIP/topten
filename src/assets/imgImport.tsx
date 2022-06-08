1ea217b889eb8259e12dd4240e5294ff265e413f
aeebbr <aeebbr@gmail.com>
2022-06-08 22:24:40 +0900

    [#12] Added scroll down icon to Ai

--- src/assets/imgImport.tsx
+++ src/assets/imgImport.tsx
@@ -8,7 +8,8 @@ import like3 from "./img/image 54.png";
 import hard1 from "./img/image 51.png";
 import hard2 from "./img/image 53.png";
 import hard3 from "./img/image 52.png";
-import spin from './img/spin.png'
+import spin from "./img/spin.png";
+import scrollDown from "./img/ScrollDown.png";
 
 export const img = {
   bin: bin,
@@ -22,4 +23,5 @@ export const img = {
   hard2: hard2,
   hard3: hard3,
   spin: spin,
+  scrollDown: scrollDown,
 };
