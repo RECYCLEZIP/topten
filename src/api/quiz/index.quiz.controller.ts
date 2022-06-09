9e791b84e417b5b97b9e4874da309c9ea3d53de8
haechan <kiki9510@gmail.com>
2022-06-10 00:57:45 +0900

    [#17] feat: 퀴즈셋 채점API controller 작성

--- src/api/quiz/index.quiz.controller.ts
+++ src/api/quiz/index.quiz.controller.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import wrapAsyncFunc from "@src/utils/catchAsync";
 import { QuizService } from "@src/service/quiz.service";
+import { STATUS_200_OK } from "@src/utils/statusCode";
+
+interface Submissions {
+    type: string;
+    answers: Object;
+}
 
 export const quizController = Router();
 const quizService = new QuizService();
@@ -28,7 +34,16 @@ quizController.get(
 // * 많이 틀린 퀴즈 순으로 상위 3개 퀴즈리스트 조회
 
 // * 퀴즈 셋 제출(타입별) => 채점결과 반환
+// * requst body = { type: "ox", answers: { quizId1: 1번문제 풀이, quizId2: 2번문제 풀이, quizId3: 3번문제 풀이, quizId4: 4번문제 풀이 }}
 // * result = { result : { quizId1 : true, quizId2 : true, quizId3 : true, quizId4 : false }, score : 75 }
+quizController.post(
+    "/quizzes/submission",
+    wrapAsyncFunc(async (req, res, next) => {
+        const { type, answers }: Submissions = req.body;
+        const result = await quizService.getResult({ type, answers });
+        return res.status(STATUS_200_OK).json(result);
+    }),
+);
 
 // * 단일 퀴즈 제출 => 채점결과 반환
 // * result = {result : { quizId : true }},
