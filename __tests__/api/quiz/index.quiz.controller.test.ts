26c1ae8efb49f212a34e0ce66330bc387814f7bd
haechan <kiki9510@gmail.com>
2022-06-12 03:47:00 +0900

    [#17] test: 퀴즈목록조회(타입) API 테스트 코드 작성

---
+++ __tests__/api/quiz/index.quiz.controller.test.ts
@@ -0,0 +1,19 @@
+import app from "@src/app";
+import request from "supertest";
+import { STATUS_200_OK } from "@src/utils/statusCode";
+
+describe("Quiz API Test", () => {
+    describe("GET /quizzes", () => {
+        it("올바른 퀴즈타입을 담아 요청하면 해당 퀴즈목록을 반환한다.", async () => {
+            const quizType = "ox";
+            const res = await request(app).get(`/quizzes?${quizType}`);
+            expect(res.statusCode).toBe(STATUS_200_OK);
+        });
+
+        it("잘못된 퀴즈타입을 담아 요청하면 빈 배열을 반환한다.", async () => {
+            const quizType = "oxx";
+            const res = await request(app).get(`/quizzes?${quizType}`).send([]);
+            expect(res.body).toEqual([]);
+        });
+    });
+});
