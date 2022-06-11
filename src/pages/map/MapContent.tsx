03ef86dcd84f70599c5239d15d905ff65b7b2896
aeebbr <aeebbr@gmail.com>
2022-06-11 13:11:45 +0900

    [#26] style: Clean up Map code

--- src/pages/map/MapContent.tsx
+++ src/pages/map/MapContent.tsx
@@ -34,7 +34,7 @@ function MapContent() {
         37.624915253753194,
         127.15122688059974,
       ),
-      // 지도 확대 레벨. 숫자 클수록 범위 넓어짐
+      // 지도 확대 레벨. 숫자 클수록 범위 멀어짐
       level: 5,
     };
 
@@ -60,18 +60,18 @@ function MapContent() {
   // 지도 이동
   const panTo = () => {
     // 이동할 위도 경도 위치 생성
-    var moveLatLon = new window.kakao.maps.LatLng(binSelected[0], binSelected[1]);
+    var moveLatLon = new window.kakao.maps.LatLng(
+      binSelected[0],
+      binSelected[1],
+    );
 
-    // 지도 중심 부드럽게 이동, 줌 인
+    // 지도 중심 부드럽게 이동, 줌 인. 한 화면에 같은 마커 있으면 이동 X
+    // 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동
+    window.map.setLevel(3);
     window.map.panTo(moveLatLon);
-    window.map.setLevel(3)
   };
 
-  return (
-    <>
-      <MapBinMapWrapper id="map"></MapBinMapWrapper>
-    </>
-  );
+  return <MapBinMapWrapper id="map"></MapBinMapWrapper>;
 }
 
 export default MapContent;
