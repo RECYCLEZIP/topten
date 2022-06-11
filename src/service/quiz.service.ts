27cac9388c88e5043622837cc6d5d48d4763bad4
haechan <kiki9510@gmail.com>
2022-06-11 16:11:07 +0900

    [#17] refactor: 단일퀴즈채점API service 코드 리팩토링

--- src/service/quiz.service.ts
+++ src/service/quiz.service.ts
@@ -8,7 +8,6 @@ interface quizSetResult {
 }
 
 export class QuizService {
-    constructor() {}
     static async getQuizList(quizType: string) {
         return await Quiz.findByQuizType(quizType);
     }
@@ -23,69 +22,53 @@ export class QuizService {
 
     // * 단일 퀴즈 채점
     static async getResult(quizId: string, answer: string) {
-        let quizResult = { result: true };
+        const quizResult = { isCorrect: true };
         const todayDate = new Date();
-        const quiz = await Quiz.findQuizById(quizId); // ? 리턴타입에 null이 왜 있는지?
-        const result = quiz!.result[0];
+        const quiz = await Quiz.findQuizById(quizId);
 
-        if (quiz === undefined) {
+        if (!quiz) {
             const errorMessage = `${quizId}에 해당하는 퀴즈 정보가 없습니다.`;
-            return { errorMessage };
+            throw new Error(errorMessage);
         }
 
-        // * 날짜 체크 -> 정답 체크(totalUser, wrong 갱신)
-        if (dayjs(todayDate).format("YYYY.MM.DD") !== dayjs(result.date).format("YYYY.MM.DD")) {
-            if (answer !== quiz!.answer) {
-                result.wrong += 1;
-                quizResult.result = false;
-            }
+        const result = quiz.result[0];
+        if (answer !== quiz.answer) {
+            result.wrong += 1;
+            quizResult.isCorrect = false;
+        }
 
+        if (dayjs(todayDate).format("YYYY.MM.DD") !== dayjs(result.date).format("YYYY.MM.DD")) {
             const wrongRate = Number((result.wrong / result.totalUser) * 100);
-            const newResult = [
-                {
-                    date: todayDate,
-                    totalUser: 1,
-                    wrong: 0,
-                    yesterday: wrongRate !== NaN ? wrongRate : 0,
-                },
-            ];
+            const newResult = {
+                date: todayDate,
+                totalUser: 1,
+                wrong: 0,
+                yesterday: isNaN(wrongRate) ? 0 : wrongRate,
+            };
+            quiz.result.push(newResult);
 
             await Quiz.updateQuizInfo(quizId, newResult);
         } else {
             result.totalUser += 1;
-            if (answer !== quiz!.answer) {
-                result.wrong += 1;
-                quizResult.result = false;
-            }
 
             await Quiz.updateQuizInfo(quizId, result);
         }
 
         return quizResult;
-
-        // * 2. 날짜 체크 후 날짜가 같으면 그대로 update
-        // * 날짜 다르면 date, yesterday 갱신, totalUser, wrong은 0으로 초기화 후 update
     }
 
-    // * 퀴즈셋 채점
+    // * 퀴즈셋 채점 -> 정답, score만 반환
     static async getResults({ type, answers }: Submissions) {
-        // * 1. 각 문제에 대한 정보 업데이트(totalUser, wrong)
-        // * 2. 각 문제 채점 결과, 총점 데이터 리턴
-        // * 3. Date가 바뀌면, result 초기화(new Date(), 0, 0, yesterday:어제 오답률)
-
-        // * 해당 타입의 문제셋의 정답 조회
         const quizAnswer = await Quiz.findAnswerByQuizType(type);
-
-        // * 문제 순회하면서 정답 체크 -> totalUser와 wrong 갱신
-        let quizSetResult: quizSetResult = { result: [], score: 0 }; // * 리턴할 데이터
+        const quizSetResult: quizSetResult = { result: [], score: 0 }; // * 리턴할 데이터
 
         const todayDate = new Date(); // * dayjs로 YYYY-MM-DD 형태로
-        const userAnwer = Object.entries(answers);
+        const userAnswer = Object.entries(answers);
 
-        for (let i = 0; i < userAnwer.length; i++) {
+        for (let i = 0; i < userAnswer.length; i++) {
             const toUpdate: ToUpdate = { totalUser: 0, wrong: 0 };
-            const quizId = userAnwer[i][0];
-            const answer: string = userAnwer[i][1];
+            const quizId = userAnswer[i][0];
+            const answer: string = userAnswer[i][1];
             const correctAnswer = quizAnswer.find((quiz) => String(quiz._id) === quizId);
 
             if (correctAnswer === undefined) {
