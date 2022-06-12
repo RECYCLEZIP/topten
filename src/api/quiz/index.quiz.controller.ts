5845d654bfb8c4945a488d72d95b867b65c16454
haechan <kiki9510@gmail.com>
2022-06-13 01:34:06 +0900

    [#32] feat: quiz API swagger-autogen 적용

--- src/api/quiz/index.quiz.controller.ts
+++ src/api/quiz/index.quiz.controller.ts
@@ -9,6 +9,18 @@ const quizController = Router();
 quizController.get(
     "/quizzes",
     wrapAsyncFunc(async (req, res, next) => {
+        /*  #swagger.tags = ["quiz"]
+            #swagger.description = "타입별 퀴즈목록 조회" 
+            #swagger.parameters['queryString'] = {
+                in: 'query',
+                description: '[multipleChoice, mixUp, ox] 3가지 타입 중 한가지로 요청',
+                required: true,
+                schema: { $ref: "#/definitions/QuizType" }
+            }    
+            #swagger.responses[200] = { 
+            schema: { "$ref": "#/definitions/QuizByType" },
+            description: "요청타입에 해당하는 퀴즈목록 조회" } */
+
         const quizType = req.query.type as string;
         const quizList = await QuizService.getQuizList(quizType);
         return res.status(STATUS_200_OK).json(quizList);
@@ -18,6 +30,13 @@ quizController.get(
 quizController.get(
     "/quizzes/wrong",
     wrapAsyncFunc(async (req, res, next) => {
+        /*  #swagger.tags = ["quiz"]
+            #swagger.description = "오답률 상위 3개 퀴즈정보 조회" 
+            
+            #swagger.responses[200] = { 
+            schema: { "$ref": "#/definitions/QuizByType" },
+            description: "오답률 상위 3개 퀴즈정보 리턴" } */
+
         const quizzesByWrongRate = await QuizService.getQuizByWrongRate();
         return res.status(STATUS_200_OK).json(quizzesByWrongRate);
     }),
@@ -26,6 +45,18 @@ quizController.get(
 quizController.get(
     "/quizzes/:id",
     wrapAsyncFunc(async (req, res, next) => {
+        /*  #swagger.tags = ["quiz"]
+            #swagger.description = "quizId에 해당하는 퀴즈정보 조회" 
+            #swagger.parameters['id'] = {
+                in: 'params',
+                description: '조회하고자 하는 퀴즈의 quizId를 경로 엔드포인트에 추가',
+                required: true,
+                schema: { $ref: "#/definitions/QuizId" }
+            }    
+            #swagger.responses[200] = { 
+            schema: { "$ref": "#/definitions/QuizByType" },
+            description: "quizId에 해당하는 퀴즈정보 조회" } */
+
         const quizId = req.params.id;
         const quiz = await QuizService.getQuiz(quizId);
         return res.status(STATUS_200_OK).json(quiz);
@@ -35,6 +66,25 @@ quizController.get(
 quizController.post(
     "/quizzes/:id/submission",
     wrapAsyncFunc(async (req, res, next) => {
+        /*  #swagger.tags = ["quiz"]
+            #swagger.description = "quizId에 해당하는 퀴즈채점" 
+            #swagger.parameters['id'] = {
+                in: 'params',
+                description: '채점하고자 하는 퀴즈의 quizId를 경로 엔드포인트에 추가',
+                required: true,
+                schema: { $ref: "#/definitions/QuizId" }
+            }
+            #swagger.parameters['answer'] = {
+                in: 'body',
+                description: '해당 퀴즈를 풀이한 유저의 답을 body에 담아 요청',
+                required: true,
+                schema: { $ref: "#/definitions/QuizUserAnswer" }
+            }
+
+            #swagger.responses[200] = { 
+            schema: { "$ref": "#/definitions/QuizResult" },
+            description: "quizId에 해당하는 퀴즈 채점결과 반환" } */
+
         const quizId = req.params.id;
         const answer: string = req.body.answer;
         const result = await QuizService.getQuizResult(quizId, answer);
@@ -45,6 +95,18 @@ quizController.post(
 quizController.post(
     "/quizzes/submission",
     wrapAsyncFunc(async (req, res, next) => {
+        /*  #swagger.tags = ["quiz"]
+            #swagger.description = "퀴즈셋 채점" 
+            #swagger.parameters['퀴즈셋정보'] = {
+                in: 'body',
+                description: '채점하고자 하는 퀴즈셋의 정보를 body에 담아 요청',
+                required: true,
+                schema: { $ref: "#/definitions/QuizSetSubmission" }
+            }    
+            #swagger.responses[200] = { 
+            schema: { "$ref": "#/definitions/QuizSetResult" },
+            description: "퀴즈셋 채점결과 반환" } */
+
         const { type, answers }: Submissions = req.body;
         const result = await QuizService.getQuizSetResults({ type, answers });
         return res.status(STATUS_200_OK).json(result);
--- swagger.js
+++ swagger.js
@@ -1,4 +1,6 @@
+const options = { autoHeaders: false, autoQuery: false, autoBody: false };
 const swaggerAutogen = require("swagger-autogen")(
+    options,
     { openapi: "3.0.0" },
     {
         language: "ko",
@@ -36,32 +38,79 @@ const doc = {
         },
     ],
     definitions: {
-        // Parents: {
-        //     father: "Simon Doe",
-        //     mother: "Marie Doe",
-        // },
-        // User: {
-        //     name: "Jhon Doe",
-        //     age: 29,
-        //     parents: {
-        //         $ref: "#/definitions/Parents",
-        //     },
-        //     diplomas: [
-        //         {
-        //             school: "XYZ University",
-        //             year: 2020,
-        //             completed: true,
-        //             internship: {
-        //                 hours: 290,
-        //                 location: "XYZ Company",
-        //             },
-        //         },
-        //     ],
-        // },
-        AddUser: {
-            $username: "haechan",
-            $password: "1234",
+        QuizId: "62a455ad6059af946a56e717",
+        QuizType: "?type=multipleChoice",
+        QuizUserAnswer: { answer: "1" },
+        QuizResult: { isCorrect: true },
+        QuizSetSubmission: {
+            type: "multipleChoice",
+            answers: [
+                {
+                    quizId: "62a163ddac4b496254c13d9e",
+                    isCorrect: true,
+                },
+                {
+                    quizId: "62a163ddac4b496254c13d9c",
+                    isCorrect: true,
+                },
+                {
+                    quizId: "62a163ddac4b496254c13da2",
+                    isCorrect: true,
+                },
+                {
+                    quizId: "62a163ddac4b496254c13da0",
+                    isCorrect: true,
+                },
+            ],
         },
+        QuizSetResult: {
+            result: [
+                {
+                    quizId: "62a163ddac4b496254c13d9e",
+                    isCorrect: true,
+                },
+                {
+                    quizId: "62a163ddac4b496254c13d9c",
+                    isCorrect: true,
+                },
+                {
+                    quizId: "62a163ddac4b496254c13da2",
+                    isCorrect: true,
+                },
+                {
+                    quizId: "62a163ddac4b496254c13da0",
+                    isCorrect: true,
+                },
+            ],
+            score: 100,
+        },
+        QuizByType: [
+            {
+                _id: "62a455ad6059af946a56e717",
+                title: "유통기한 지난 의약품의 분리배출 방법으로 옳은 것을 고르시오.",
+                description:
+                    "폐의약품을 그냥 버릴 경우 환경오염을 일으킬 수 있다. 약국에 비치된 폐의약품 수거함에 버린다.",
+                options: [
+                    "일반쓰레기로 버린다.",
+                    "약국에 비치된 폐의약품 수거함에 버린다.",
+                    "내 뱃속에 버린다.",
+                    "약사님께 드린다.",
+                ],
+                answer: "1",
+                type: "multipleChoice",
+                image: "https://images.unsplash.com/photo-1607619056574-7b8d3ee536b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1840&q=80",
+                result: [
+                    {
+                        date: "2022-06-11T08:43:25.335Z",
+                        totalUser: 2,
+                        wrong: 1,
+                        yesterday: 0,
+                        _id: "62a455ad6059af946a56e718",
+                    },
+                ],
+                __v: 0,
+            },
+        ],
     },
 };
 
