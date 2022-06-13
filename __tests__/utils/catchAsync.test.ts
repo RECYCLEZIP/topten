cc665ad5b1c256d9cc87714981b416cf7043c011
Handwoong <handwoong@gmail.com>
2022-06-13 21:36:58 +0900

    test: mocking RequestHandler 모듈적용

--- __tests__/utils/catchAsync.test.ts
+++ __tests__/utils/catchAsync.test.ts
@@ -1,22 +1,10 @@
-import { RequestHandler, Request, Response } from "express";
+import { RequestHandler } from "express";
 import wrapAsyncFunc from "@src/utils/catchAsync";
-
-const mockRequest = (): Request => {
-    const req: unknown = jest.fn();
-    return req as Request;
-};
-
-const mockResponse = (): Response => {
-    const res: unknown = {
-        status: jest.fn(() => res),
-        send: jest.fn(),
-    };
-    return res as Response;
-};
+import { mockRequest, mockResponse, mockNext } from "@src/utils/setUpTests";
 
 const req = mockRequest();
 const res = mockResponse();
-const mockNext = jest.fn();
+const next = mockNext();
 
 describe("에러가 발생하면 에러를 캐치한다.", () => {
     let testValue = true;
@@ -26,13 +14,13 @@ describe("에러가 발생하면 에러를 캐치한다.", () => {
     };
 
     it("Request 콜백함수에서 에러가 발생하면 next 함수를 호출한다.", () => {
-        wrapAsyncFunc(mockCallback)(req, res, mockNext);
-        expect(mockNext).toBeCalledTimes(1);
+        wrapAsyncFunc(mockCallback)(req, res, next);
+        expect(next).toBeCalledTimes(1);
     });
 
     it("Request 콜백함수가 정상 실행되면 next 함수가 호출되지 않는다.", () => {
         testValue = false;
-        wrapAsyncFunc(mockCallback)(req, res, mockNext);
-        expect(mockNext).toBeCalledTimes(0);
+        wrapAsyncFunc(mockCallback)(req, res, next);
+        expect(next).toBeCalledTimes(0);
     });
 });
