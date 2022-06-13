126f9c15e45ca74870bca4970f6697e6c10cc49d
ms <altjsvnf@naver.com>
2022-06-13 21:27:26 +0900

    feat: react-helmet

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import { RecoilRoot } from "recoil";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
