83033cdb46f0117b0be173d740095d5e79cf75bb
ms <altjsvnf@naver.com>
2022-06-13 15:17:02 +0900

    chore: api

--- src/api.tsx
+++ src/api.tsx
@@ -2,11 +2,8 @@ import axios from "axios";
 
 const serverUrl = process.env.REACT_APP_SERVER_URL + "/";
 
-export async function getData(endpoint: string, params = "") {
-  console.log(
-    `%cGET 요청 ${serverUrl + endpoint + "/" + params}`,
-    "color: #a25cd1;",
-  );
+export async function getData(endpoint: string) {
+  console.log(`%cGET 요청 ${serverUrl + endpoint}`, "color: #a25cd1;");
 
-  return axios.get(serverUrl + endpoint + "/" + params);
+  return axios.get(serverUrl + endpoint);
 }
