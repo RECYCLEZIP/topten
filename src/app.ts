647575cd33092c7e0e762804910d43ba7f7eb10f
Handwoong <handwoong@gmail.com>
2022-06-15 07:43:03 +0900

    chore: 세션, JWT, bcrypt 설치

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import session from "express-session";
 import { errorMiddleware } from "@src/middlewares/errorHandler";
 import { indexController } from "@src/api/index.controller";
 import "dotenv/config";
@@ -9,6 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: "devKey",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    }),
+);
 
 indexController(app);
 app.use(errorMiddleware);
