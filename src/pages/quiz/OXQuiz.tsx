870e064862eb99fdc04a9adcf65b6370190e26cf
ms <altjsvnf@naver.com>
2022-06-16 00:35:40 +0900

    chore: ox page

--- src/pages/quiz/OXQuiz.tsx
+++ src/pages/quiz/OXQuiz.tsx
@@ -4,13 +4,15 @@ import {
   TwoOption,
   TwoOptions,
 } from "../../styles/quizStyles/QuizzesStyle";
-import QuestionCard from "./QuestionCard";
 import ClearIcon from "@mui/icons-material/Clear";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
-import Answer from "./Answer";
 import { getData } from "../../api";
-import { useSetRecoilState } from "recoil";
-import { quizListState } from "../../stores/atoms";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import {
+  answerState,
+  quizListState,
+  selectedAnswerState,
+} from "../../stores/atoms";
 
 // ox quiz page component
 function OXQuiz() {
@@ -19,9 +21,11 @@ function OXQuiz() {
     <CircleOutlinedIcon style={{ fontSize: "2.5rem" }} />,
     <ClearIcon style={{ fontSize: "3.5rem" }} />,
   ];
+  const resultOption = ["O", "X"];
   //recoil로 빼도록 하자.
-  const [isSelected, setIsSelected] = useState([false]);
+  const [isSelected, setIsSelected] = useRecoilState(selectedAnswerState);
   const setQuizzes = useSetRecoilState(quizListState);
+  const setOption = useSetRecoilState(answerState);
 
   // selected option toggle true
   const clickHandler = (idx: number) => {
@@ -42,6 +46,11 @@ function OXQuiz() {
   useEffect(() => {
     getQuiz();
   }, []);
+
+  useEffect(() => {
+    const answer = isSelected.indexOf(true);
+    setOption(resultOption[answer]);
+  }, [isSelected]);
 
   return (
     <>
