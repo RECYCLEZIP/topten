e64b5083f08de4f6007832fa1e2ae60eb40f7b8f
haechan <kiki9510@gmail.com>
2022-06-17 02:53:52 +0900

    [#42] feat: bins.service 작성

---
+++ src/repository/bins.repository.ts
@@ -0,0 +1,5 @@
+export class Bins {
+    static async findBinsLocation({ filteredQuery, limit }) {
+        return;
+    }
+}
--- src/service/bins.service.ts
+++ src/service/bins.service.ts
@@ -1,5 +1,19 @@
 import { FilterQuery } from "@src/models/interface";
+import { createFilterQuery } from "@src/utils/createQuery";
+import { Bins } from "@src/repository/bins.repository";
+import { RequestError } from "@src/middlewares/errorHandler";
+import { STATUS_404_NOTFOUND } from "@src/utils/statusCode";
 
 export class BinsService {
-    static async getBinsLocation(query: FilterQuery) {}
+    static async getBinsLocation(query: FilterQuery) {
+        const filterList = ["region", "roads"];
+        const { filteredQuery, limit } = createFilterQuery(query, filterList);
+        const locationList = await Bins.findBinsLocation({ filteredQuery, limit });
+        if (!locationList)
+            throw new RequestError(
+                "해당 조건에 일치하는 쓰레기통 위치정보가 없습니다.",
+                STATUS_404_NOTFOUND,
+            );
+        return locationList;
+    }
 }
