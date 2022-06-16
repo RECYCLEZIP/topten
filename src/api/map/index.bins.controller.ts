fc95d34830f36a5498947675c708200175c78d14
haechan <kiki9510@gmail.com>
2022-06-17 02:10:13 +0900

    [#42] feat: bins.controller, 쓰레기통위치검색API 작성

---
+++ src/api/map/index.bins.controller.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import wrapAsyncFunc from "@src/utils/catchAsync";
+import { STATUS_200_OK } from "@src/utils/statusCode";
+import { BinsService } from "@src/service/bins.service";
+// import { paramsValidator } from "@src/middlewares/requestValidator";
+
+const binsController = Router();
+
+binsController.get(
+    "/bins",
+    wrapAsyncFunc(async (req, res, next) => {
+        const binsLocation = await BinsService.getBinsLocation(req.query);
+        return res.status(STATUS_200_OK).json(binsLocation);
+    }),
+);
---
+++ src/service/bins.service.ts
@@ -0,0 +1,5 @@
+import { FilterQuery } from "@src/models/interface";
+
+export class BinsService {
+    static async getBinsLocation(query: FilterQuery) {}
+}
