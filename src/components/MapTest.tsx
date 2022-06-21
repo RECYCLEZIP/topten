7b153ede93f52840e6c283e0a0cf6bfd10c9f8b0
aeebbr <aeebbr@gmail.com>
2022-06-21 18:49:44 +0900

    [#15] chore: Change the center coordinate setting so that all markers come out

--- src/components/MapTest.tsx
+++ src/components/MapTest.tsx
@@ -13,6 +13,7 @@ declare global {
 }
 
 function MapTest({
+  type,
   props,
   propsSelected,
   setSelectedMarker,
@@ -22,6 +23,8 @@ function MapTest({
   // const propsSelected = useRecoilValue(BinSelectedState);
   // const setSelectedMarker = useSetRecoilState(selectedMarkerState);
   // const setPropsSelected = useSetRecoilState(BinSelectedState);
+
+  // console.log(type, props, propsSelected, setSelectedMarker, setPropsSelected);
 
   useEffect(() => {
     // 렌더링 후 지도 띄우기
@@ -33,14 +36,16 @@ function MapTest({
     panTo();
   }, [propsSelected]);
 
+  // var bounds = new window.kakao.maps.LatLngBounds();
+
   // 지도 띄우기 - 다중 마커
   const mapLoad = () => {
     // 지도 띄울 element
     let container = document.getElementById("map");
 
     let options = {
-      // 지도 중심좌표
-      center: new window.kakao.maps.LatLng(37.5764804046, 126.9709050229),
+      // 지도 중심좌표(모든 마커가 나오도록 하단에서 재설정)
+      center: new window.kakao.maps.LatLng(0, 0),
       // 지도 확대 레벨. 숫자 클수록 멀어짐
       level: 6,
     };
@@ -50,6 +55,9 @@ function MapTest({
     // 지도 줌 컨트롤
     var zoomControl = new window.kakao.maps.ZoomControl();
     window.map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+
+    // 중심 좌표 범위정보
+    var bounds = new window.kakao.maps.LatLngBounds();
 
     props.forEach((prop: any) => {
       // 마커 생성
@@ -70,6 +78,10 @@ function MapTest({
         });
         setPropsSelected([prop.x, prop.y]);
       });
+
+      // 모든 마커가 나오도록 중심좌표 설정
+      bounds.extend(new window.kakao.maps.LatLng(prop.y, prop.x));
+      window.map.setBounds(bounds);
     });
   };
 
@@ -89,7 +101,7 @@ function MapTest({
   };
 
   return (
-    <MapContentContainer>
+    <MapContentContainer type={type}>
       <MapContentWrapper id="map"></MapContentWrapper>
     </MapContentContainer>
   );
