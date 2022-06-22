574aab31e1583fbf0b94a1f14320f84d2a38796c
aeebbr <aeebbr@gmail.com>
2022-06-22 22:15:55 +0900

    [#47] feat: Created qna

---
+++ src/pages/qna/QnA.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import {
+  BarSection,
+  BarText,
+  BarRedText,
+  ListTable,
+  ListTbody,
+  ListTr,
+  ListNumber,
+  ListTitle,
+  ListAuthor,
+  ListDate,
+  ListAnswerLabel,
+} from "../../styles/qnaStyles/QnAStyle";
+
+import { TitleText } from "../../styles/TextStyle";
+import { Container } from "../../styles/basicStyle";
+
+function QnA() {
+  return (
+    <Container>
+      {/* 페이지 타이틀 */}
+      <TitleText>Q&A</TitleText>
+      {/* 바 섹션 */}
+      <BarSection>
+        {/* 전체 게시글 수 */}
+        <BarText>
+          전체 <BarRedText>12</BarRedText>건
+        </BarText>
+        {/* 페이지 번호 */}
+        <BarText>
+          페이지 <BarRedText>1</BarRedText>
+          /32
+        </BarText>
+        {/* 검색  */}
+        <div></div>
+      </BarSection>
+      {/* 게시글 리스트 섹션 */}
+      <ListTable>
+        {/* 리스트 tbody */}
+        <ListTbody>
+          <ListTr>
+            {/* 페이지 번호 */}
+            <ListNumber>12</ListNumber>
+            <ListTitle>어떻게?ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹㅇㄴㄹㅇㄴㄹㄴㅇㄹ</ListTitle>
+            <ListAuthor>강*선</ListAuthor>
+            <ListDate>2022.01.10</ListDate>
+            <ListAnswerLabel>답변완료</ListAnswerLabel>
+          </ListTr>
+          <ListTr>
+            {/* 페이지 번호 */}
+            <ListNumber>12</ListNumber>
+            <ListTitle>어떻게?</ListTitle>
+            <ListAuthor>강*선</ListAuthor>
+            <ListDate>2022.01.10</ListDate>
+            <ListAnswerLabel>답변완료</ListAnswerLabel>
+          </ListTr>
+          <ListTr>
+            {/* 페이지 번호 */}
+            <ListNumber>12</ListNumber>
+            <ListTitle>어떻게?</ListTitle>
+            <ListAuthor>강*선</ListAuthor>
+            <ListDate>2022.01.10</ListDate>
+            <ListAnswerLabel>답변완료</ListAnswerLabel>
+          </ListTr>
+        </ListTbody>
+      </ListTable>
+      {/* 페이지네이션 */}
+      <div></div>
+    </Container>
+  );
+}
+
+export default QnA;
---
+++ src/styles/qnaStyles/QnAStyle.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const BarSection = styled.section`
+  display: inline-flex;
+`;
+
+export const BarText = styled.div`
+  margin-right: 1rem;
+  font-size: 0.6rem;
+`;
+
+export const BarRedText = styled.span`
+  font-size: 0.6rem;
+  color: #a62121;
+`;
+
+export const ListSection = styled.section``;
+
+export const ListTable = styled.table`
+  width: 100%;
+`;
+
+export const ListTbody = styled.tbody`
+  font-size: 0.6rem;
+`;
+
+export const ListTr = styled.tr`
+  margin: 0.5rem 0rem;
+
+  /* display: flex; */
+`;
+
+export const ListNumber = styled.td`
+  color: #979797;
+`;
+
+export const ListTitle = styled.td``;
+
+export const ListAuthor = styled.td`
+  color: #979797;
+`;
+
+export const ListDate = styled.td`
+  color: #979797;
+`;
+
+export const ListAnswerLabel = styled.td``;
