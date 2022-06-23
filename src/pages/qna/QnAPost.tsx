e4c3690af3cd6660a8d1bf42d7930fd859cd8d30
aeebbr <aeebbr@gmail.com>
2022-06-24 00:48:19 +0900

    [#48] feat: Renamed QnAWirte -> QnAPost, Added font color

--- src/pages/qna/QnAPost.tsx
+++ src/pages/qna/QnAPost.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
 
+import { getData } from "../../api";
+
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
+
+import "tui-color-picker/dist/tui-color-picker.css";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
 
 import { Container } from "../../styles/basicStyle";
 import { TitleText } from "../../styles/TextStyle";
 
-function QnAWrite() {
+function QnAPost() {
   return (
     <Container>
       <TitleText>QnA 작성</TitleText>
@@ -16,9 +22,10 @@ function QnAWrite() {
         height="600px"
         initialEditType="markdown"
         useCommandShortcut={true}
+        plugins={[colorSyntax]} // colorSyntax 플러그인 적용
       />
     </Container>
   );
 }
 
-export default QnAWrite;
+export default QnAPost;
