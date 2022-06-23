550c1422b57ae1e6a11028ea3561ab0b62a6d05d
haechan <kiki9510@gmail.com>
2022-06-23 18:52:31 +0900

    [#61] feat: robot schema location 필드 수정

--- seeds/insertionRobotData.ts
+++ seeds/insertionRobotData.ts
@@ -11,7 +11,8 @@ interface RobotData {
 }
 
 const DB_URL =
-    process.env.MONGODB_URL || "MongoDB 서버 주소가 설정되지 않았습니다. env 파일을 확인해 주세요.";
+    "mongodb+srv://topten:topten10@cluster0.nfefn.mongodb.net/green-DB?retryWrites=true&w=majority";
+// process.env.MONGODB_URL || "MongoDB 서버 주소가 설정되지 않았습니다. env 파일을 확인해 주세요.";
 
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
@@ -30,6 +31,7 @@ db.on("connected", async () => {
             const robotDataList = robotData.map((data: RobotData) => {
                 return {
                     name: data.title,
+                    address: data.address,
                     location: {
                         type: "Point",
                         coordinates: [Number(data.y), Number(data.x)],
