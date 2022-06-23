c1a0c52938e1ee9f4b971f81c0c1ffe705c7f5e3
haechan <kiki9510@gmail.com>
2022-06-24 00:41:39 +0900

    [#61] chore: robot service에 빈 결과값에 대한 예외처리 추가

--- src/service/robot.service.ts
+++ src/service/robot.service.ts
@@ -1,4 +1,6 @@
+import { RequestError } from "@src/middlewares/errorHandler";
 import { Robot } from "@src/repository/robot.repository";
+import { STATUS_404_NOTFOUND } from "@src/utils/statusCode";
 
 export class RobotService {
     static async getCloseRobots(x: string, y: string) {
@@ -15,6 +17,12 @@ export class RobotService {
         };
 
         const robotsLocation = await Robot.findLocation(geoQuery);
+        if (robotsLocation.length === 0) {
+            throw new RequestError(
+                "해당 반경 내 순환자원 회수로봇의 위치정보가 없습니다.",
+                STATUS_404_NOTFOUND,
+            );
+        }
         return robotsLocation;
     }
 }
