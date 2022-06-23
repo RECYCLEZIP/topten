098a54b8c285ec83a48215f1f4ef2b44899d6a57
haechan <kiki9510@gmail.com>
2022-06-23 20:41:57 +0900

    [#61] feat: robot service 코드 작성

--- src/api/map/index.robot.controller.ts
+++ src/api/map/index.robot.controller.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import wrapAsyncFunc from "@src/utils/catchAsync";
+import { RobotService } from "@src/service/robot.service";
 import { STATUS_200_OK } from "@src/utils/statusCode";
 
 const robotController = Router();
@@ -7,7 +8,9 @@ const robotController = Router();
 robotController.get(
     "/robot",
     wrapAsyncFunc(async (req, res, next) => {
-        const closeRobotList = await RobotService.getCloseRobots(req.query);
+        const x = req.query.x as string;
+        const y = req.query.y as string;
+        const closeRobotList = await RobotService.getCloseRobots(x, y);
         res.status(STATUS_200_OK).json(closeRobotList);
     }),
 );
---
+++ src/service/robot.service.ts
@@ -0,0 +1,19 @@
+export class RobotService {
+    static async getCloseRobots(x: string, y: string) {
+        const geoQuery = {
+            location: {
+                $nearSphere: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [x, y],
+                    },
+                    $minDistance: 1000,
+                    $maxDistance: 10000,
+                },
+            },
+        };
+
+        const robotsLocation = await Robot.findLocation(geoQuery);
+        return robotsLocation;
+    }
+}
