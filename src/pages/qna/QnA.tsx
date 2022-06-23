206927cedf1f741bee68376b5c0003f59ff9de2e
aeebbr <aeebbr@gmail.com>
2022-06-24 00:13:13 +0900

    [#47] chore: Separated Qna components

--- src/pages/qna/QnA.tsx
+++ src/pages/qna/QnA.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-import {
-  BarSection,
-  BarText,
-  BarRedText,
-  ListTable,
-  ListTbody,
-  ListTr,
-  ListNumber,
-  ListTitle,
-  ListAuthor,
-  ListDate,
-} from "../../styles/qnaStyles/QnAStyle";
+import QnABar from "./QnABar";
+import QnAList from "../../pages/qna/QnAList";
 
 import { TitleText } from "../../styles/TextStyle";
 import { Container } from "../../styles/basicStyle";
@@ -22,46 +12,9 @@ function QnA() {
       {/* 페이지 타이틀 */}
       <TitleText>Q&A</TitleText>
       {/* 바 섹션 */}
-      <BarSection>
-        {/* 전체 게시글 수 */}
-        <BarText>
-          전체 <BarRedText>12</BarRedText>건
-        </BarText>
-        {/* 페이지 번호 */}
-        <BarText>
-          페이지 <BarRedText>1</BarRedText>
-          /32
-        </BarText>
-        {/* 검색  */}
-        <div></div>
-      </BarSection>
+      <QnABar />
       {/* 게시글 리스트 섹션 */}
-      <ListTable>
-        {/* 리스트 tbody */}
-        <ListTbody>
-          <ListTr>
-            {/* 페이지 번호 */}
-            <ListNumber>12</ListNumber>
-            <ListTitle>어떻게? 하는 거</ListTitle>
-            <ListAuthor>강*선</ListAuthor>
-            <ListDate>2022.01.10</ListDate>
-          </ListTr>
-          <ListTr>
-            {/* 페이지 번호 */}
-            <ListNumber>12</ListNumber>
-            <ListTitle>어떻게?</ListTitle>
-            <ListAuthor>강*선</ListAuthor>
-            <ListDate>2022.01.10</ListDate>
-          </ListTr>
-          <ListTr>
-            {/* 페이지 번호 */}
-            <ListNumber>12</ListNumber>
-            <ListTitle>어떻게?</ListTitle>
-            <ListAuthor>강*선</ListAuthor>
-            <ListDate>2022.01.10</ListDate>
-          </ListTr>
-        </ListTbody>
-      </ListTable>
+      <QnAList />
       {/* 페이지네이션 */}
       <div></div>
     </Container>
