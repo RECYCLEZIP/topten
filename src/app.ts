be34e8000872a1134783d5317f260fd6dcd34700
Handwoong <handwoong@gmail.com>
2022-06-25 16:06:50 +0900

    [#69] fix: 대용량 문자열 에러 express limit 설정

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ import { indexController } from "@src/api/index.controller";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 indexController(app);
