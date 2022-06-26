c0d49ebe6874eed1243526f9bb8200cd31858f45
aeebbr <aeebbr@gmail.com>
2022-06-26 16:18:51 +0900

    [#55] feat: QnA Search

--- src/pages/qna/QnABar.tsx
+++ src/pages/qna/QnABar.tsx
@@ -2,39 +2,92 @@ import React, { useState, useEffect } from "react";
 
 import { getData } from "../../api";
 
-import { useRecoilState } from "recoil";
-import { QnAListState } from "../../stores/atoms";
+import { useRecoilState, useResetRecoilState } from "recoil";
+import {
+  QnAListState,
+  QnASearchState,
+  QnASearchValueState,
+} from "../../stores/atoms";
 
 import {
   BarSection,
   BarText,
   BarRedText,
+  SearchContainer,
+  SearchInput,
 } from "../../styles/qnaStyles/QnAStyle";
 
 function QnABar() {
-  const [qnaList, setQnaList] = useRecoilState(QnAListState);
+  const [qnaAllList, setQnaAllList] = useRecoilState(QnAListState);
+  const [qnaSearchList, setQnaSearchList] = useRecoilState(QnASearchState);
+
+  const [searchValue, setSearchValue] =
+    useRecoilState<string>(QnASearchValueState);
+
+  const resetQnASearch = useResetRecoilState(QnASearchState);
 
   const getList = async () => {
     try {
-      await getData(`posts`).then((res) => setQnaList(res.data));
+      await getData(`posts`).then((res) => setQnaAllList(res.data));
     } catch (err) {
       console.log(err);
     }
   };
 
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onKeyPressEnter = (e: any) => {
+    // 엔터키가 눌렸을 때
+    if (e.key === "Enter") {
+      if (searchValue !== "") {
+        // 검색어 있을 시
+        const searchResult = qnaAllList.filter((qna) => {
+          console.log(searchValue);
+          return qna.title.includes(searchValue);
+        });
+
+        console.log(searchResult);
+        setQnaSearchList(searchResult);
+      } else {
+        // 검색어 없을 시
+        console.log("검색어 없음");
+        resetQnASearch();
+        // setQnaSearchList(null);
+      }
+    }
+  };
+
   useEffect(() => {
     getList();
+    // enterkey();
   }, []);
 
   return (
     <BarSection>
+      {/* <div> */}
       <BarText>
-        전체 <BarRedText>{qnaList.length}</BarRedText>건
+        전체 <BarRedText>{qnaAllList.length}</BarRedText>건
       </BarText>
       <BarText>
         페이지 <BarRedText>1</BarRedText>
         /32
       </BarText>
+      {/* </div> */}
+      <SearchContainer>
+        {/* <SearchWrapper> */}
+        <SearchInput
+          id="search"
+          type="text"
+          placeholder="검색어를 입력해주세요."
+          value={searchValue}
+          onKeyPress={onKeyPressEnter}
+          // onKeyPress={if( event.keyCode==13 ){addFunc();}}
+          onChange={onSearchChange}
+        ></SearchInput>
+        {/* </SearchWrapper> */}
+      </SearchContainer>
     </BarSection>
   );
 }
--- src/pages/qna/QnAList.tsx
+++ src/pages/qna/QnAList.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { useRecoilValue } from "recoil";
-import { QnAListState } from "../../stores/atoms";
+import {
+  QnAListState,
+  QnASearchState,
+  QnASearchValueState,
+} from "../../stores/atoms";
+
+import { QnAType } from "../../types/QnA";
 
 import { useNavigate } from "react-router";
 
@@ -9,6 +15,7 @@ import {
   ListTable,
   ListTbody,
   ListTr,
+  NothingTd,
   ListNumber,
   ListTitle,
   ListAuthor,
@@ -20,27 +27,59 @@ import { BlackHr } from "../../styles/qnaStyles/QnADescriptionStyle";
 function QnAList() {
   const navigate = useNavigate();
 
-  const qnaList = useRecoilValue(QnAListState);
+  const qnaAllList = useRecoilValue(QnAListState);
+  const [qnaList, setQnaList] = useState<QnAType | any>();
+  const qnaSearchList = useRecoilValue(QnASearchState);
+  const searchValue = useRecoilValue<string>(QnASearchValueState);
 
   const date = (prop: any) => {
     return prop.split("T")[0].split("-").join(".");
   };
+
+  useEffect(() => {
+    // 검색 전 전체 리스트 세팅
+    setQnaList([...qnaAllList]);
+  }, [qnaAllList]);
+
+  useEffect(() => {
+    if (searchValue !== "") {
+      // 검색어 있을 시
+      if (qnaSearchList.length !== 0) {
+        // 검색 결과 있을 시
+        setQnaList(qnaSearchList);
+      } else {
+        // 검색 결과 없을 시
+        setQnaList([]);
+      }
+    } else {
+      // 검색어 없을 시
+      setQnaList(qnaAllList);
+    }
+  }, [qnaSearchList]);
 
   return (
     <>
       <BlackHr />
       <ListTable>
         <ListTbody>
-          {qnaList.map((qna, idx) => (
-            <ListTr>
-              {/* 게시글 번호 내림차순으로 */}
-              <ListNumber>{qnaList.length - idx}</ListNumber>
-              <ListTitle onClick={() => navigate(`/qna/${qna._id}`)}>
-                {qna?.title}
-              </ListTitle>
-              <ListAuthor>{qna?.author?.username}</ListAuthor>
-              <ListDate>{date(qna?.createdAt)}</ListDate>
-            </ListTr>
+          {qnaAllList.map((qna: any, idx: any) => (
+            <>
+              {qnaList?.length === 0 ? (
+                <tr>
+                  <NothingTd>조회된 게시물이 없습니다.</NothingTd>
+                </tr>
+              ) : (
+                <ListTr>
+                  {/* 게시글 번호 내림차순으로 */}
+                  <ListNumber>{qnaAllList.length - idx}</ListNumber>
+                  <ListTitle onClick={() => navigate(`/qna/${qna._id}`)}>
+                    {qna?.title}
+                  </ListTitle>
+                  <ListAuthor>{qna?.author?.username}</ListAuthor>
+                  <ListDate>{date(qna?.createdAt)}</ListDate>
+                </ListTr>
+              )}
+            </>
           ))}
         </ListTbody>
       </ListTable>
