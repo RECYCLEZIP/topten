c213e9cc5a387c330cbedcf42cb83be8f4825967
aeebbr <aeebbr@gmail.com>
2022-06-26 16:19:09 +0900

    [#55] feat: Styled QnA search

--- src/styles/qnaStyles/QnAStyle.tsx
+++ src/styles/qnaStyles/QnAStyle.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const BarSection = styled.section`
   display: inline-flex;
+
+  width: 100%;
 `;
 
 export const BarText = styled.div`
@@ -12,6 +14,37 @@ export const BarText = styled.div`
 export const BarRedText = styled.span`
   font-size: 0.6rem;
   color: #a62121;
+`;
+
+export const SearchContainer = styled.div`
+  width: 20%;
+
+  margin-left: auto;
+
+  display: flex;
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+
+  padding: 0.1rem 0.8rem;
+  line-height: 1rem;
+
+  /* border: 1px solid #dadde6; */
+  border: 1px solid #4f4f4f;
+  border-radius: 0.8rem;
+
+  font-size: 0.53rem;
+
+  &:hover {
+    background: #f7f9fa;
+    /* border: 1px solid #dadde6; */
+  }
+
+  &:focus {
+    border-color: #69db7c;
+    outline: none;
+  }
 `;
 
 export const ListSection = styled.section``;
@@ -34,6 +67,13 @@ export const ListTr = styled.tr`
   &:last-child {
     border-bottom: none;
   }
+`;
+
+export const NothingTd = styled.td`
+  padding: 0.8rem 0.5rem;
+
+  text-align: center;
+  font-weight: 500;
 `;
 
 export const ListNumber = styled.td`
