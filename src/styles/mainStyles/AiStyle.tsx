76228b2879c130ea2aee262ae15f7eddad461490
ms <altjsvnf@naver.com>
2022-06-28 11:52:32 +0900

    [#60] chore: ai section

--- src/styles/mainStyles/AiStyle.tsx
+++ src/styles/mainStyles/AiStyle.tsx
@@ -2,14 +2,17 @@ import styled from "styled-components";
 
 export const AiContainer = styled.div`
   background-color: #69db7c;
-  padding: 5% 0;
+  padding: 1.3rem 0;
   text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+  flex-direction: column;
 
-export const AiDescription = styled.div``;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
 
 export const AiImg = styled.img`
   width: 50%;
