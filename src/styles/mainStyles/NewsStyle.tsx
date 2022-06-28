80ed6dde46a3c479d88718a0e2a33e324df26cd3
ms <altjsvnf@naver.com>
2022-06-28 11:53:33 +0900

    [#60] chore: main page news section

--- src/styles/mainStyles/NewsStyle.tsx
+++ src/styles/mainStyles/NewsStyle.tsx
@@ -4,6 +4,7 @@ export const Container = styled.div`
   margin: 2% 8% 0;
   display: flex;
   align-items: center;
+  flex-wrap: wrap;
 
   @media (min-width: 768px) {
     flex-direction: row;
@@ -19,7 +20,7 @@ export const NewsText = styled.span`
 export const NewsTitle = styled.a`
   display: flex;
   text-decoration: none;
-  font-size: 0.6rem;
+  font-size: 0.5rem;
   color: #9eacba;
   height: 100%;
 `;
