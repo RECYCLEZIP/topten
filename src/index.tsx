94982f7830a502319c91a6e1c1418dfb1db0b979
Handwoong <handwoong@gmail.com>
2022-06-29 23:29:42 +0900

    [#66] feat: index.tsx에 hydrate 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { hydrate, render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,18 +7,28 @@ import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import { RecoilRoot } from "recoil";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-
-root.render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </BrowserRouter>,
-);
+const rootElement = document.getElementById("root")!;
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <BrowserRouter>
+      <ScrollToTop />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </BrowserRouter>,
+    rootElement,
+  );
+} else {
+  render(
+    <BrowserRouter>
+      <ScrollToTop />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </BrowserRouter>,
+    rootElement,
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
