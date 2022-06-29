3ae7497bbecfaa99a08a4a39f767b1b5871eb6c1
aeebbr <aeebbr@gmail.com>
2022-06-29 12:29:46 +0900

    [#56] feat: Created UserAllQnaList

---
+++ src/pages/user/UserAllQnAList.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+
+import { useRecoilValue, useRecoilState } from "recoil";
+import {
+  QnAListState,
+  QnASearchState,
+  QnASearchValueState,
+  QnAPageState,
+  QnALengthState,
+  QnANumPagesState,
+} from "../../stores/atoms";
+
+import { QnAType } from "../../types/QnA";
+
+import { useNavigate } from "react-router";
+
+import {
+  ListTable,
+  ListTbody,
+  ListTr,
+  NothingTd,
+  ListNumber,
+  ListTitle,
+  ListAuthor,
+  ListDate,
+} from "../../styles/qnaStyles/QnAStyle";
+
+import { BlackHr } from "../../styles/qnaStyles/QnADescriptionStyle";
+
+function UserAllQnAList() {
+  const navigate = useNavigate();
+
+  const qnaAllList = useRecoilValue(QnAListState);
+  const [qnaList, setQnaList] = useState<QnAType | any>();
+
+  const [qnaTotal, setQnaTotal] = useRecoilState(QnALengthState);
+  const [qnaPage, setQnaPage] = useRecoilState(QnAPageState);
+
+  const [numPages, setNumPages] = useRecoilState(QnANumPagesState);
+
+  const offset = (qnaPage - 1) * 10;
+
+  const date = (prop: string) => {
+    return prop.split("T")[0].split("-").join(".");
+  };
+
+  useEffect(() => {
+    // 검색 전 전체 리스트 세팅
+    console.log(qnaAllList);
+    setQnaList([...qnaAllList]);
+  }, [qnaAllList]);
+
+  return (
+    <>
+      <BlackHr />
+      <ListTable>
+        <ListTbody>
+          {/* {qnaList?.slice(offset, offset + 10).map((qna: any, idx: number) => ( */}
+          {qnaList?.map((qna: any, idx: number) => (
+            <>
+              <>{console.log(qnaPage)}</>
+              {qnaList?.length === 0 ? (
+                <tr>
+                  <NothingTd>조회된 게시물이 없습니다.</NothingTd>
+                </tr>
+              ) : (
+                <ListTr>
+                  {/* 게시글 번호 내림차순으로 */}
+                  <ListNumber>{qnaList.length - idx}</ListNumber>
+                  <ListTitle onClick={() => navigate(`/qna/${qna._id}`)}>
+                    {qna?.title}
+                  </ListTitle>
+                  <ListDate>{date(qna?.createdAt)}</ListDate>
+                </ListTr>
+              )}
+            </>
+          ))}
+        </ListTbody>
+      </ListTable>
+      <BlackHr />
+    </>
+  );
+}
+
+export default UserAllQnAList;
